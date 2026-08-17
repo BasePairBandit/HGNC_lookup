c345bd11066c6b8deaf6e1f499e9f8b19e2372a6
KannaS <sachin.kannangaraarachchige@gosh.nhs.uk>
2026-08-17 15:07:59 +0000

    Implemented the validation and logic for the search

--- hgnc_lookup_app/views.py
+++ hgnc_lookup_app/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .services.gene_search import (
+    validate_search_query,
+    UserInputError,
+    find_by_symbol,
+    find_by_hgnc_id,
+)
+
+def search_gene(request):
+
+    query = request.GET.get("query", "")
+
+    result = None
+    error = None
+
+    if query:
+
+        try:
+            query = validate_search_query(query)
+
+            if query.upper().startswith("HGNC:"):
+                result = find_by_hgnc_id(query)
+            else:
+                result = find_by_symbol(query)
+
+            if result is None:
+                error = "Gene not found."
+
+        except UserInputError as exc:
+            error = str(exc)
+
+    return render(
+        request,
+        "hgnc_lookup_app/search.html",
+        {
+            "gene": result,
+            "error": error,
+        }
+    )
