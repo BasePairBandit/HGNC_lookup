2b039a1f0e6df4fbeffe75b6fcff8e62b97aa0a8
KannaS <sachin.kannangaraarachchige@gosh.nhs.uk>
2026-08-21 13:52:15 +0000

    added more tests to reach 100% coverage of gene_search functions

--- hgnc_lookup_app/tests/test_gene_search.py
+++ hgnc_lookup_app/tests/test_gene_search.py
@@ -1,6 +1,25 @@
 import pytest
-from hgnc_lookup_app.services.gene_search import find_by_symbol, find_by_hgnc_id
+from hgnc_lookup_app.services.gene_search import find_by_symbol, find_by_hgnc_id, validate_search_query, UserInputError
 from hgnc_lookup_app.services.data_loader import load_dataset
+
+
+def test_validate_search_query_normal_case():
+    result = validate_search_query("APOE")
+    assert result is not None
+    assert result[0:4]=="APOE"
+
+def test_validate_search_query_invalid_character(caplog):
+
+    with pytest.raises(UserInputError):
+        validate_search_query("APO/")
+    assert "User entered invalid input: APO/" in caplog.text
+
+def test_validate_search_query_empty(caplog):
+
+    with pytest.raises(UserInputError):
+        validate_search_query("")
+    assert "Empty user input" in caplog.text
+
 
 def test_find_by_symbol_normal_case():
 
@@ -8,6 +27,19 @@ def test_find_by_symbol_normal_case():
 
     assert result is not None
     assert result["gene_symbol"]=="APOE"
+
+def test_find_by_symbol_non_existent_symbol():
+
+    result = find_by_symbol("AAAA")
+
+    assert result is None
+
+def test_find_by_symbol_valid_hyphen():
+
+    result = find_by_symbol("A1BG-AS1")
+
+    assert result is not None
+    assert result["gene_symbol"]=="A1BG-AS1"
 
 def test_find_by_symbol_case_insensitive():
 
@@ -41,3 +73,23 @@ def test_find_by_hgnc_id_case_insensitive():
 
     assert result is not None
     assert result["hgnc_id"]=="HGNC:613"
+
+def test_find_by_hgnc_id_space():
+
+    result = find_by_hgnc_id("HGNC :613")
+
+    assert result is not None
+    assert result["hgnc_id"]=="HGNC:613"
+
+def test_find_by_hgnc_id_mixed_case():
+
+    result = find_by_hgnc_id("HGnC :613")
+
+    assert result is not None
+    assert result["hgnc_id"]=="HGNC:613"
+
+def test_find_by_hgnc_id_non_existent_id():
+
+    result = find_by_hgnc_id("HGnC :000")
+
+    assert result is None
