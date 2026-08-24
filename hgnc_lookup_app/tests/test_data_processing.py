0668f836a2e7b6c7742c841e0da808fd3f08c6b5
KannaS <sachin.kannangaraarachchige@gosh.nhs.uk>
2026-08-24 15:00:36 +0000

    Added doc strings explaining the tests

--- hgnc_lookup_app/tests/test_data_processing.py
+++ hgnc_lookup_app/tests/test_data_processing.py
@@ -3,9 +3,15 @@ from hgnc_lookup_app.services.data_loader import DATA_FILE, load_dataset
 
 
 def test_data_file_exists():
+    """
+    Testing if the file exists
+    """
     assert DATA_FILE.exists
 
 def test_load_dataset():
+    """
+    Testing that the dataset is not empty and is a list
+    """
     result = load_dataset()
     assert result is not None
     assert isinstance(result, list)
