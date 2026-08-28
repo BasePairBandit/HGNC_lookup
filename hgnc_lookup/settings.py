152b6bcbf12890340a11fbfc9b22dc91fbf5b1f0
KannaS <sachin.kannangaraarachchige@gosh.nhs.uk>
2026-08-28 14:25:23 +0000

    Moved SECRET_KEY to environment variable and ignore .env file

--- hgnc_lookup/settings.py
+++ hgnc_lookup/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pyr@_=3x@jzcm2&5xw0=emfn42_xxf=9ccv7m)6x8ua9)bb9#b"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-key-for-dev")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
